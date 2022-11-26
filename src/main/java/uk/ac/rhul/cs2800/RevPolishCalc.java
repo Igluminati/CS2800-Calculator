9c9af908e576b485d9a00f227098636b49773d1a
Igli Kristo <zjac300@live.rhul.ac.uk>
2022-11-26 14:01:20 +0000

    Created class NumStack since reverse Polish Evaluation only uses a stack of numeric values

--- src/main/java/uk/ac/rhul/cs2800/RevPolishCalc.java
+++ src/main/java/uk/ac/rhul/cs2800/RevPolishCalc.java
@@ -2,6 +2,8 @@ package uk.ac.rhul.cs2800;
 
 public class RevPolishCalc {
 
+  NumStack values;
+  
   public float evaluate(String expression) {
     // TODO Auto-generated method stub
     return 0;
