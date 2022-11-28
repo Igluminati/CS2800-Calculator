16d66b937c0d1776fcbbe6e35b1947f5459a8941
Igli Kristo <zjac300@live.rhul.ac.uk>
2022-11-28 13:59:23 +0000

    A new method is created to push a float value to Stack by storing the float in the Entry class

--- src/main/java/uk/ac/rhul/cs2800/NumStack.java
+++ src/main/java/uk/ac/rhul/cs2800/NumStack.java
@@ -3,5 +3,16 @@ package uk.ac.rhul.cs2800;
 public class NumStack {
 	
 	Stack numstack;
+
+	/*
+	 * New instance of the Stack class is created
+	 */
+	public NumStack() {
+		numstack = new Stack();
+	}
+	
+	public void push(Entry value) {
+		numstack.push(value);
+	}
 	
 }
