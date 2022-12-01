fc138eb7ff1762bd7f0d552c21f7a7f56b3f983a
Igli Kristo <zjac300@live.rhul.ac.uk>
2022-12-01 02:40:24 +0000

    A new method pop() is created to return the value of the float which has been popped off the stack: NumStack

--- src/main/java/uk/ac/rhul/cs2800/NumStack.java
+++ src/main/java/uk/ac/rhul/cs2800/NumStack.java
@@ -1,5 +1,7 @@
 package uk.ac.rhul.cs2800;
 
+import java.util.EmptyStackException;
+
 public class NumStack {
 	
 	Stack numstack;
@@ -17,6 +19,15 @@ public class NumStack {
 
 	public int size() {
 		return numstack.size();
+	}
+
+	public float pop() throws Exception {
+		try {
+			return numstack.pop().getValue();
+		} catch (Exception e) {
+			System.err.println(e);
+		}
+		return 0f;
 	}
 	
 	
