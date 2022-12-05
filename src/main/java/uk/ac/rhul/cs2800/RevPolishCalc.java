3e8b6657800081526e03e94f7350faa9495cddce
Igli Kristo <zjac300@live.rhul.ac.uk>
2022-12-05 18:24:36 +0000

    calculate() method calculates two numStack values using the given operator symbol

--- src/main/java/uk/ac/rhul/cs2800/RevPolishCalc.java
+++ src/main/java/uk/ac/rhul/cs2800/RevPolishCalc.java
@@ -18,5 +18,23 @@ public class RevPolishCalc implements Calculator {
     return 0;
   }
 
+  public float calculate(float first, float second, Symbol symbol) {
+	  switch (symbol) {
+	  	case MULTIPLY:
+	  		return (float) first * second;
+	  	case DIVIDE:
+	  		if(second == 0) {
+	  			throw new ArithmeticException("Cannot divide by zero!");
+	  		}
+	  		return first / second;
+	  	case PLUS:
+	  		return first + second;
+	  	case MINUS:
+	  		return first - second;
+	  	default:
+	  		return 0f;
+  }
+}
+
    
 }
