81fb35878d14910438296fd0c399b1a7d59e183f
Igli Kristo <zjac300@live.rhul.ac.uk>
2022-12-05 16:18:01 +0000

    RevPolishCalc class now implements the Calculator interface

--- src/main/java/uk/ac/rhul/cs2800/RevPolishCalc.java
+++ src/main/java/uk/ac/rhul/cs2800/RevPolishCalc.java
@@ -1,9 +1,18 @@
 package uk.ac.rhul.cs2800;
 
-public class RevPolishCalc {
+/**
+ * RevPolishCalc implements the 'evaluate()' abstract method in the 'Calculator' interface,
+ * which is overriden to allow for expressions in Reverse Polish Calculation to be evaluated.
+ * 
+ * 
+ *  @author Igli Kristo zjac300 
+ */
+
+public class RevPolishCalc implements Calculator {
 
   NumStack values;
   
+  @Override
   public float evaluate(String expression) {
     // TODO Auto-generated method stub
     return 0;
