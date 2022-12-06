7f685162a5dca3a0e5bd17ca925b302722f3521e
Igli Kristo <zjac300@live.rhul.ac.uk>
2022-12-06 19:01:40 +0000

    pushing values of type Symbol - test successful

--- src/main/java/uk/ac/rhul/cs2800/OpStack.java
+++ src/main/java/uk/ac/rhul/cs2800/OpStack.java
@@ -2,4 +2,20 @@ package uk.ac.rhul.cs2800;
 
 public class OpStack {
 
+	Stack opstack;
+
+	/*
+	 * New instance of the Stack class is created
+	 */
+	public OpStack() {
+		opstack = new Stack();
+	}
+	
+	public void push(Symbol symbol) {
+		opstack.push(new Entry(symbol));
+	}
+	
+	public int size() {
+		return opstack.size();
+	}
 }
