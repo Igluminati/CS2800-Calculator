9a570fece5ad2d8c0af0a08919d13f23f50405ef
Igli Kristo <zjac300@live.rhul.ac.uk>
2022-12-08 20:47:23 +0000

    CalcModel class allows for expressions in both infix and postfix form to be passed along with a boolean argument to indicate which form the expression is written in

--- src/main/java/uk/ac/rhul/cs2800/CalcModel.java
+++ src/main/java/uk/ac/rhul/cs2800/CalcModel.java
@@ -1,15 +1,29 @@
 package uk.ac.rhul.cs2800;
 
+/* Igli Kristo zjac300 */
+
 public class CalcModel {
 	
 	Calculator revPolish;
 	Calculator Standard;
+	
+	/* 
+	 * Constructs two instances to allow expressions in both infix
+	 * and postfix form to be evaluated.
+	*/
 	
 	public CalcModel() {
 		revPolish = new RevPolishCalc();
 		Standard = new StandardCalc();
 	}
 
+	/*
+	 * The evaluate method takes two arguments: The String expression
+	 * and a boolean infix to indicate whether the expression is
+	 * infix (true) or postfix ()
+	 * 
+	 */
+	
 	public float evaluate(String expression, boolean infix) {
 		if (infix == true) {
 			return Standard.evaluate(expression);
